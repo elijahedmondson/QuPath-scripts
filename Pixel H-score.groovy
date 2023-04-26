8563515ef29b7bd74fcc6f33ed4add8f748fa091
Elijah Edmondson <elijah.edmondson@gmail.com>
2023-04-26 07:59:38 -0400

    Pixel H-score

--- Pixel H-score.groovy
+++ Pixel H-score.groovy
@@ -1,85 +1,75 @@
-//SET THESE
-DABthresholds=[0.15,0.5,1] //1+, 2+, 3+ DAB
-Hthreshold=0.08 //Nuclei in Hematoxylin
-double scale = 1 //bigger = downsample for speed 
-def annotClass="Region*" //what class are your annotations?
+import ij.IJ
+import qupath.lib.images.servers.ImageServer
+import qupath.lib.images.servers.TransformedServerBuilder
+import qupath.lib.regions.RegionRequest
+import qupath.lib.scripting.QP
+import java.awt.image.BufferedImage
+import qupath.imagej.tools.IJTools
+import ij.ImageJ
+import ij.ImagePlus
+import ij.plugin.ImageCalculator
 
-//make pixel_classifiers folder
-String pixelClassifierFolder=buildFilePath(PROJECT_BASE_DIR,'classifiers','pixel_classifiers') //project subfolder with pixel classifier
-mkdirs(pixelClassifierFolder)
+double[] DABthresholds=[0.15,0.5,1] //1+, 2+, 3+ DAB
+double Hthreshold=0.08 //Nuclei in Hematoxylin
+def downsample = 1
 
-//name classes and classifiers
-String[] classes = ["1+", "2+", "3+"]
-String[] classifiers = ["1.json", "2.json", "3.json"]
+ImageServer<BufferedImage> myserver = QP.getCurrentServer() as ImageServer<BufferedImage>
+def originalPixelSize = myserver.getPixelCalibration().getAveragedPixelSizeMicrons()
+def imageData = QP.getCurrentImageData()
+def tsb = new TransformedServerBuilder(myserver).deconvolveStains(imageData.getColorDeconvolutionStains()).build()
+def annotations = QP.getSelectedObjects()
 
+/*
+def ij = new ImageJ()
+ij.setVisible(true)
+ */
+IJ.setForegroundColor(255,255,255)
+IJ.setBackgroundColor(0,0,0)
+annotations.forEach({annot ->
+    def roi = annot.getROI()
+    def region = RegionRequest.createInstance(myserver.getPath(), downsample, roi)
+    def pthimgplus = IJTools.convertToImagePlus(tsb, region)
+    def imgplus = pthimgplus.getImage()
+    def ijroi = IJTools.convertToIJRoi(roi, pthimgplus)
+    imgplus.setRoi(ijroi, true)
+    //imgplus.show()
 
-//get pixel calibration and current stain vectors
-imageData = getCurrentImageData()
-PixelCalibration cal=imageData.getServer().getPixelCalibration()
+    def imstack = imgplus.getStack()
+    def heam_proc = imstack.getProcessor(1)
+    heam_proc.fillOutside(ijroi)
+    heam_proc.setThreshold(Hthreshold, 1e3)
+    def mask_h = heam_proc.createMask()
+    def dab_proc = imstack.getProcessor(2)
+    dab_proc.fillOutside(ijroi)
+    dab_proc.setThreshold(DABthresholds[0], 1e3)
+    def mask_d0 = dab_proc.createMask()
+    dab_proc.setThreshold(DABthresholds[1], 1e3)
+    def mask_d1 = dab_proc.createMask()
+    dab_proc.setThreshold(DABthresholds[2], 1e3)
+    def mask_d2 = dab_proc.createMask()
 
-def stains = getCurrentImageData().getColorDeconvolutionStains()
+    // create combined mask
+    def himageplus = new ImagePlus("hmask", mask_h)
+    def dimageplus = new ImagePlus("dmask", mask_d0)
+    def ormask = ImageCalculator.run(himageplus, dimageplus, "or")
+    def totpix_stat =  ormask.getStatistics()
+    def dab0_stat = mask_d0.getStatistics()
+    def dab1_stat = mask_d1.getStatistics()
+    def dab2_stat = mask_d2.getStatistics()
 
-//create pixel classifiers for DAB thresholds
-for (i=0; i<3; i++){
+    def pix_tot = totpix_stat.mean * totpix_stat.area / 255
+    def pix_d0 = dab0_stat.mean * dab0_stat.area / 255
+    def pix_d1 = dab1_stat.mean * dab1_stat.area / 255
+    def pix_d2 = dab2_stat.mean * dab2_stat.area / 255
 
-    //define preprocessing and thresholds
-    def ops2 = [
-        ImageOps.Channels.deconvolve(stains), 
-        ImageOps.Channels.extract(1), //H=0, DAB=1
-      //  ImageOps.Filters.gaussianBlur(1), //1x gaussian smoothing
-        ImageOps.Threshold.threshold(DABthresholds[i]), //threshold for DAB channel
-    ]
-    def op = ImageOps.buildImageDataOp().appendOps(*ops2) //turn into single ImageOp
-
-    Map classmap=Map.of(1,getPathClass(classes[i])) //define intensity 1 = positive
-    pixclass=PixelClassifiers.createClassifier(op,cal.createScaledInstance(scale,scale),classmap) //create the classifier
-
-    //write new classifier to file
-    Path writepath=Path.of(pixelClassifierFolder,classifiers[i])
-    PixelClassifiers.writeClassifier(pixclass,writepath)
-    
-    //measure the area of each class
-    selectObjectsByClassification(annotClass);
-    addPixelClassifierMeasurements((i+1).toString(), (i+1).toString())
-}
-
-//for the H score denominator, get all the pixels that are either DAB-positive or Hematoxylin-positive
-//important not to double count double-positive pixels
-def opsdouble = [
-        ImageOps.Channels.deconvolve(stains),
-        ImageOps.Channels.extract(0,1), //choose the channels for thresholding (0-based)
-      //  ImageOps.Filters.gaussianBlur(1), //1x gaussian smoothing
-        ImageOps.Threshold.threshold(Hthreshold,DABthresholds[0]), //threshold for each channel, matching the order of the extraction
-        ImageOps.Channels.maximum()  //maximum creates the union of the two thresholds
-]
-def opdouble = ImageOps.buildImageDataOp().appendOps(*opsdouble)
-
-Map classmap=Map.of(1,getPathClass('HDAB')) //define intensity 1 = newPathClass
-pixclass=PixelClassifiers.createClassifier(opdouble,cal.createScaledInstance(scale,scale),classmap) //create the classifier
-
-//write new classifier to file
-Path writepath=Path.of(pixelClassifierFolder,'HDAB.json')
-PixelClassifiers.writeClassifier(pixclass,writepath)
-
-selectObjectsByClassification(annotClass);
-addPixelClassifierMeasurements("HDAB", "HDAB") //measure H-DAB class
-
-def annots=getAnnotationObjects().findAll{it.getPathClass()==getPathClass(annotClass)} //get objects to measure
-
-//measure area of each class
-annots.each{annot->
-    double area1=annot.measurements["1: 1+ area µm^2"]
-    double area2=annot.measurements["2: 2+ area µm^2"]
-    double area3=annot.measurements["3: 3+ area µm^2"]
-    double areaDenom=annot.measurements["HDAB: HDAB area µm^2"]
-
-annot.measurements['Pixelwise H-score']=(area1+area2+area3)/(areaDenom)*100 //calculate H score
-//NOTE: 1+ area INCLUDES 2+ and 3+. Therefore area1 + area2 + area3 = 1*DAB_low + 2*DAB_med + 3*DAB_high.
-//no need for subtraction and re-addition
-}
-
-import qupath.lib.images.servers.PixelCalibration
-import qupath.opencv.ml.pixel.PixelClassifiers
-import qupath.opencv.ops.ImageOps
-import java.nio.file.Path
-import static qupath.lib.gui.scripting.QPEx.*
+    print("tot " + pix_tot)
+    print("d0 " + pix_d0)
+    print("d1 " + pix_d1)
+    print("d2 " + pix_d2)
+    def pixhscore = 100 * (pix_d0+pix_d1+pix_d2 )/pix_tot
+    annot.measurements['Pixelwise H-score IJ']=pixhscore
+    annot.measurements["1: 1+ area µm^2 IJ"] = pix_d0 * originalPixelSize * originalPixelSize
+    annot.measurements["2: 2+ area µm^2 IJ"] = pix_d1 * originalPixelSize * originalPixelSize
+    annot.measurements["3: 3+ area µm^2 IJ"] = pix_d2 * originalPixelSize * originalPixelSize
+    annot.measurements["HDAB: HDAB area µm^2 IJ"] = pix_tot * originalPixelSize * originalPixelSize
+})
