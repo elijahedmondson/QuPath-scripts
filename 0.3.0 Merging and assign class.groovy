ee6ee4c31a8ec7b7aba702eb0a075b4a7368c501
Elijah Edmondson <elijah.edmondson@gmail.com>
2023-06-12 16:38:37 -0400

    merg

--- 0.3.0 Merging and assign class.groovy
+++ 0.3.0 Merging and assign class.groovy
@@ -1,24 +1,24 @@
 
 selectObjects {it.isAnnotation() && it.getPathClass() == null};
 mergeSelectedAnnotations()
-resultingClass = getPathClass("Glomeruli")
+resultingClass = getPathClass("Tumor")
 toChange = getAnnotationObjects().findAll{it.getPathClass() == null}
 toChange.each{ it.setPathClass(resultingClass)}
 
-addPixelClassifierMeasurements("DAB_0.3", "DAB_0.3")
+//addPixelClassifierMeasurements("DAB_0.3", "DAB_0.3")
 
-import qupath.lib.gui.measure.ObservableMeasurementTableData
-def ob = new ObservableMeasurementTableData();
-ob.setImageData( getCurrentImageData(),  getAnnotationObjects() );
+//import qupath.lib.gui.measure.ObservableMeasurementTableData
+//def ob = new ObservableMeasurementTableData();
+//ob.setImageData( getCurrentImageData(),  getAnnotationObjects() );
 
-getAnnotationObjects().each{
-    pos_pixel = ob.getNumericValue(it, "DAB_0.3: Positive area µm^2")
-    total_pixel = ob.getNumericValue(it, "Area µm^2")
-    
-    perc_pos_pixel = pos_pixel/total_pixel*100
-    it.getMeasurementList().putMeasurement("% Positive Pixel", perc_pos_pixel)
-    print perc_pos_pixel
- }
+//getAnnotationObjects().each{
+//    pos_pixel = ob.getNumericValue(it, "DAB_0.3: Positive area µm^2")
+//    total_pixel = ob.getNumericValue(it, "Area µm^2")
+//    
+//    perc_pos_pixel = pos_pixel/total_pixel*100
+//    it.getMeasurementList().putMeasurement("% Positive Pixel", perc_pos_pixel)
+//    print perc_pos_pixel
+// }
  
  
  Thread.sleep(100)
--- 0.3.0 Merging.groovy
+++ 0.3.0 Merging.groovy
@@ -2,4 +2,10 @@
 //selectAnnotations();
 mergeSelectedAnnotations();
 
-
+Thread.sleep(100)
+// Try to reclaim whatever memory we can, including emptying the tile cache
+javafx.application.Platform.runLater {
+    getCurrentViewer().getImageRegionStore().cache.clear()
+    System.gc()
+}
+Thread.sleep(100)
