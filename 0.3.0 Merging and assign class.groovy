6d8548f49b93fe1e66f1c05a9cce0b504b2c9ff5
Elijah Edmondson <elijah.edmondson@gmail.com>
2023-07-06 09:16:37 -0400

    Update 0.3.0 Merging and assign class.groovy

--- 0.3.0 Merging and assign class.groovy
+++ 0.3.0 Merging and assign class.groovy
@@ -1,25 +1,10 @@
 
 selectObjects {it.isAnnotation() && it.getPathClass() == null};
 mergeSelectedAnnotations()
-resultingClass = getPathClass("Tumor")
+resultingClass = getPathClass("Glomeruli")
 toChange = getAnnotationObjects().findAll{it.getPathClass() == null}
 toChange.each{ it.setPathClass(resultingClass)}
 
-//addPixelClassifierMeasurements("DAB_0.3", "DAB_0.3")
-
-//import qupath.lib.gui.measure.ObservableMeasurementTableData
-//def ob = new ObservableMeasurementTableData();
-//ob.setImageData( getCurrentImageData(),  getAnnotationObjects() );
-
-//getAnnotationObjects().each{
-//    pos_pixel = ob.getNumericValue(it, "DAB_0.3: Positive area µm^2")
-//    total_pixel = ob.getNumericValue(it, "Area µm^2")
-//    
-//    perc_pos_pixel = pos_pixel/total_pixel*100
-//    it.getMeasurementList().putMeasurement("% Positive Pixel", perc_pos_pixel)
-//    print perc_pos_pixel
-// }
- 
  
  Thread.sleep(100)
 // Try to reclaim whatever memory we can, including emptying the tile cache
